111d10822bb87cebe37f3ffe0fa638bb39458444
Adrien <adriensamie8@gmail.com>
2026-02-26 23:49:11 +0100

    fix supabase webhook build

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import { Sidebar } from '@/components/layout/sidebar'
 import { Header } from '@/components/layout/header'
+export const dynamic = 'force-dynamic'
 
 export default async function AppLayout({
   children,
