4a4e181d8ead54058bdddeafd4960df25fb23671
Adrien <adriensamie8@gmail.com>
2026-02-27 18:58:35 +0100

    fix commandes-client export

--- app/(auth)/sign-in/[[...rest]]/page.tsx
+++ app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', background: '#04080f' }}>
+    <div className="flex items-center justify-center min-h-screen">
       <SignIn />
     </div>
   )
